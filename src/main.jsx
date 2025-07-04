ffe659c7d2931d40b69c20368e851f4ab79e35ac
danielPanawan853 <danadaniel897@gmail.com>
2025-07-04 13:21:26 +0700

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import Footer from './components/Footer.jsx';
-import PreLoader from './components/PreLoader.jsx';
 import 'remixicon/fonts/remixicon.css';
 import 'animate.css';
 import AOS from 'aos';
